518ba3de8f8be6e5872eb4de5a5611601caef521
NumberI <x0987654321@ya.ru>
2020-03-10 21:35:41 +0300

    lesson 70

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,36 +1,35 @@
 import React, { Component } from "react";
 import Header from "../app-header";
 import RandomPlanet from "../random-planet";
-import ItemList from "../item-list";
-import PersonDetails from "../person-details";
 
 import "./App.css";
+import ErrorIndicator from "../error-indicator";
+import PeoplePage from "../people-page";
 
 export default class App extends Component {
   state = {
     showRandomPlanet: true,
-    selectedPerson: 2
+    hasError: false
   };
 
   toggleRandomPlanet = () => {
-    this.setState((state) => {
+    this.setState(state => {
       return {
         showRandomPlanet: !state.showRandomPlanet
-      }
+      };
     });
   };
 
-  onPersonSelected = (id) => {
-    this.setState({
-      selectedPerson: id
-    });
-  };
+  componentDidCatch() {
+    console.log("compcatch");
+    this.setState({ hasError: true });
+  }
 
   render() {
-
-    const planet = this.state.showRandomPlanet ?
-      <RandomPlanet /> :
-      null;
+    if (this.state.hasError) {
+      return <ErrorIndicator />;
+    }
+    const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
 
     return (
       <div className="stardb-app">
@@ -39,19 +38,14 @@ export default class App extends Component {
 
         <button
           className="toggle-planet btn btn-warning btn-lg"
-          onClick={this.toggleRandomPlanet}>
+          onClick={this.toggleRandomPlanet}
+        >
           Toggle Random Planet
         </button>
-
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList onItemSelected={this.onPersonSelected} />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson} />
-          </div>
-        </div>
+        <PeoplePage />
+        <PeoplePage />
+        <PeoplePage />
       </div>
     );
   }
-};
+}
